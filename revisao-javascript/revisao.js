67694bada0e202638e7fe70c0ad83eb3cb0e5e03
50304ENDEX <lucasbarbosa4373@gmail.com>
2025-02-28 20:35:00 -0300

    second commint

---
+++ revisao-javascript/revisao.js
@@ -0,0 +1,16 @@
+// Variaveis 
+const nome = "Lucas";
+let idade = 20;
+const maiordeidade = true;
+
+
+console.log(nome)
+console.log(idade)
+console.log(maiordeidade)
+
+
+nome = "teste"
+idade = 18
+
+console.log(nome)
+console.log(idade)
